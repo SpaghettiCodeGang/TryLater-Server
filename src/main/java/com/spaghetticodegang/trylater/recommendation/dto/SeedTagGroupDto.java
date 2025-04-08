738520d63be0bba60cffc145ec7426909fb9efa1
covox <lehmann.s7@gmx.de>
2025-04-08 10:21:29 +0200

    feat: adds db seed for categories, tag groups and tags

---
+++ src/main/java/com/spaghetticodegang/trylater/recommendation/dto/SeedTagGroupDto.java
@@ -0,0 +1,13 @@
+package com.spaghetticodegang.trylater.recommendation.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class SeedTagGroupDto {
+    private String tagGroupName;
+    private List<String> tags;
+}
