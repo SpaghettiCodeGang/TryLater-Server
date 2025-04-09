ff7895c1de52d5b47d6a0f576f10249488f2d5b0
covox <lehmann.s7@gmx.de>
2025-04-09 15:27:18 +0200

    feat: adds tag service and dtos to create tag response dtos

---
+++ src/main/java/com/spaghetticodegang/trylater/recommendation/tag/TagController.java
@@ -0,0 +1,13 @@
+package com.spaghetticodegang.trylater.recommendation.tag;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller providing endpoints for tag management.
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommendation/tag")
+public class TagController {}
