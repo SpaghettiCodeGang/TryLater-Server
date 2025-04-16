cfb1b6dc7664e166fa5c5ef60c73d45dd4b1ee91
Phruxy <142888294+Phruxy@users.noreply.github.com>
2025-04-17 00:29:36 +0200

    feat: add logic to request all contact for user with optional filter for status and request contact by id

--- src/main/java/com/spaghetticodegang/trylater/contact/ContactController.java
+++ src/main/java/com/spaghetticodegang/trylater/contact/ContactController.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * REST controller providing endpoints for managing user contacts.
  */
@@ -59,5 +61,28 @@ public class ContactController {
     public ResponseEntity<Void> deleteContact(@AuthenticationPrincipal User me, @PathVariable("id") Long contactId) {
         contactService.deleteContact(me, contactId);
         return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Returns a contact or contact request by delegating the request to the service layer.
+     *
+     * @param me        the currently authenticated user
+     * @param contactId the ID of the contact whose is requested
+     * @return the created contact as a response DTO
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<ContactResponseDto> getContactById(@AuthenticationPrincipal User me, @PathVariable("id") Long contactId) {
+        return ResponseEntity.ok(contactService.getContact(me, contactId));
+    }
+
+    /**
+     * Returns all contact or contact request for a user by delegating the request to the service layer.
+     *
+     * @param me the currently authenticated user
+     * @return the created contact as a response DTO
+     */
+    @GetMapping
+    public ResponseEntity<List<ContactResponseDto>> getAllContacts(@AuthenticationPrincipal User me, @RequestParam(name = "contactStatus", required = false) ContactStatus contactStatus) {
+        return ResponseEntity.ok(contactService.getAllContacts(me, contactStatus));
     }
 }
