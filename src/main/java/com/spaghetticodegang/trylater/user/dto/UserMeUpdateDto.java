067a8081cda3bac83fd36f4dcd234168fa595920
Phruxy <142888294+Phruxy@users.noreply.github.com>
2025-04-20 17:19:01 +0200

    fix: delete blank validation in user me update dto

--- src/main/java/com/spaghetticodegang/trylater/user/dto/UserMeUpdateDto.java
+++ src/main/java/com/spaghetticodegang/trylater/user/dto/UserMeUpdateDto.java
@@ -1,7 +1,6 @@
 package com.spaghetticodegang.trylater.user.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
