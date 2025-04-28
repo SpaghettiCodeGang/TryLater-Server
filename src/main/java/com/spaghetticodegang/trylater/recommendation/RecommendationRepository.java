b0155c5f8e23046ccb8d7f8061005ef93bdd58f0
covox <lehmann.s7@gmx.de>
2025-04-28 08:12:58 +0200

    fix: changes CrudRepository to JpaRepository

--- src/main/java/com/spaghetticodegang/trylater/recommendation/RecommendationRepository.java
+++ src/main/java/com/spaghetticodegang/trylater/recommendation/RecommendationRepository.java
@@ -1,6 +1,7 @@
 package com.spaghetticodegang.trylater.recommendation;
 
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,7 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 /**
  * Repository interface for accessing and managing recommendation entities in the database.
  */
-public interface RecommendationRepository extends CrudRepository<Recommendation, Long> {
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
 
     /**
      * In case of a user is deleted, updates the creator to null
